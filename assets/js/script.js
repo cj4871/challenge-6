0683036d574aa78a87aea6c18f6b5a4c2da380fb
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-07-12 22:01:45 -0500

    added design and small details in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 const srchbtn = document.getElementById('searchButton')
 const apiKey = 'e5e0ae10ea57a4b5d58bfa3d3dfe819a'
+const TWDisplay = document.getElementById('todaysWeather')
+const fiveDWDisplay = document.getElementById('5dayWeather')
 
 function runWeatherApi() {
     const userInput = document.getElementById('search').value
     weatherAPI(userInput);
 }
 
+function display(){
+    TWDisplay.style.border = "2px solid black"
+    TWDisplay.style.width = "auto"
+    TWDisplay.style.marginRight = "20%"
+    TWDisplay.style.alignContent = "center"
+    TWDisplay.style.fontSize = "20pt"
+    TWDisplay.style.float = "right"
+    TWDisplay.style.borderRadius = "10px"
+    
+}
 
 const weatherAPI = function (city){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&cnt=10&units=imperial`)
     .then((res)=>{
         console.log(res)
         return res.json()
@@ -17,23 +29,27 @@ const weatherAPI = function (city){
         console.log(data.main.temp)
         console.log(data.wind.speed)
         console.log(data.main.humidity)
+        console.log(data.temp)
         function runWeatherDisplay(){
-            const TWDisplay = document.getElementById('todaysWeather')
-            const fiveDWDisplay = document.getElementById('5dayWeather')
             const cityDisplay= document.createElement('h2')
             const temp = document.createElement('h3')
             const wind = document.createElement('h3')
             const humidity = document.createElement('h3');
             cityDisplay.innerText = city
-            temp.innerText = data.main.temp
-            wind.innerText = data.wind.speed
-            humidity.innerText = data.main.humidity
+            temp.innerText = `Temp: ${data.main.temp}f`
+            wind.innerText = `Wind Speed: ${data.wind.speed}mph`
+            humidity.innerText = `Humidity: ${data.main.humidity}%`
             TWDisplay.appendChild(cityDisplay)
             TWDisplay.appendChild(temp)
             TWDisplay.appendChild(wind)
             TWDisplay.appendChild(humidity)
+
         }
         runWeatherDisplay(city)
+        display()
+        // function runFiveDay(){
+        //     let 
+        // }
 
     }) 
     if (!runWeatherApi) {
